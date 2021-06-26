cf5088c4e62b26f747326593c1dcc1de9d58cf5f
Tim Paine <t.paine154@gmail.com>
2021-06-26 14:25:46 -0400

    fix lint

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import { Route, HashRouter, Switch, Redirect} from 'react-router-dom'
-import Main from './components/Main'
-import ScrollToTop from './components/ScrollTop'
+import React from 'react';
+import {
+  Route, HashRouter, Switch, Redirect,
+} from 'react-router-dom';
+import Main from './components/Main';
+import ScrollToTop from './components/ScrollTop';
 
-
-export default props => (
-    <HashRouter>
-      <ScrollToTop>
-        <Switch>
-          <Route exact path='/' component={ Main } />
-          <Route component={ () => <Redirect to="/" /> } />
-        </Switch>
-      </ScrollToTop>
-    </HashRouter>
-  )
+export default () => (
+  <HashRouter>
+    <ScrollToTop>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route component={() => <Redirect to="/" />} />
+      </Switch>
+    </ScrollToTop>
+  </HashRouter>
+);
