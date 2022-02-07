ecab9401444875f270d0016db5c371e31eb84144
Tim Paine <t.paine154@gmail.com>
2022-02-07 11:27:18 -0500

    fix lint

--- src/routes.js
+++ src/routes.js
@@ -3,16 +3,16 @@ import {Route, HashRouter, Switch, Redirect} from "react-router-dom";
 import Main from "./components/Main";
 import ScrollToTop from "./components/ScrollTop";
 
-export default function () {
-  return (
-    <HashRouter>
-      <ScrollToTop>
-        <Switch>
-          <Route exact path="/" component={Main} />
-          {/* eslint-disable-next-line react/no-unstable-nested-components */}
-          <Route component={() => <Redirect to="/" />} />
-        </Switch>
-      </ScrollToTop>
-    </HashRouter>
-  );
-}
+const routes = () => (
+  <HashRouter>
+    <ScrollToTop>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        {/* eslint-disable-next-line react/no-unstable-nested-components */}
+        <Route component={() => <Redirect to="/" />} />
+      </Switch>
+    </ScrollToTop>
+  </HashRouter>
+);
+
+export default routes;
